aea65674894f3446705c7e27e9e1aaf0025d16eb
mikazee <meruertzm@gmail.com>
2025-05-16 19:47:30 +0500

    Homework #5

--- src/screens/pokemon-details/index.tsx
+++ src/screens/pokemon-details/index.tsx
@@ -1,14 +1,5 @@
 import PokemonDetails from '../../components/PokemonDetails/PokemonDetails.tsx'
 import { useLoaderData } from 'react-router-dom'
-import { mockPokemons } from '../../data/pokemonsData.tsx'
-
-export const pokemonDetailsLoader = () => {
-  const pokemon = mockPokemons
-  if (!pokemon) {
-    throw new Error('No pokemon found')
-  }
-  return pokemon
-}
 
 const PokemonDetailsPage = () => {
   const pokemon = useLoaderData() as PokemonDetails
--- src/screens/pokemons-list/pokemonsLoader.ts
+++
@@ -1,9 +0,0 @@
-export async function pokemonsListLoader() {
-  const res = await fetch('https://pokeapi.co/api/v2/pokemon?limit=20')
-
-  if (!res.ok) {
-    throw new Error('Could not fetch pokemons')
-  }
-  const pokemons = await res.json()
-  return pokemons
-}
